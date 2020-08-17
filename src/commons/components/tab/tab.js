9e92960e5ee4ba80e4da83c110a107952deb1424
laviniustan <laviniu.stan@gmail.com>
2020-08-17 23:59:33 +0300

    tab/ css

--- src/commons/components/tab/tab.js
+++ src/commons/components/tab/tab.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import './tab-style.css'
 
+const Tab = ({element, index, onClick, onChange}) => {
 
-const Tab =({element, index,onClick,onChange})=>{
-
-    
     return (
-        
+
         <div className='tab'>
-            
-            <div  className={`${onChange===index?'active':''} `}>
-            {
-                <p onClick={()=>onClick(index)}>{element}</p>
+
+            <div
+                className={`tab__element ${onChange === index
+                    ? 'active'
+                    : ''} `}>
+                {<p onClick={() => onClick(index)}>{element}</p>}
+
+                {
+                    onChange === index
+                        ? <p></p>
+                        : ''
                 }
-                
-                {onChange===index?<p>activ</p>:''}
 
             </div>
-         
+
         </div>
     )
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,31 +1,44 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import BookCard from '../../commons/components/book-card/book-card'
 import Tab from '../../commons/components/tab/tab'
+import './home-style.css'
 
-const tab=['All Genres', 'Business','Science','Fiction','Philosophy','Biography']
-  
+const tab = [
+    'All Genres',
+    'Business',
+    'Science',
+    'Fiction',
+    'Philosophy',
+    'Biography'
+]
 
+const Home = () => {
+    const [activeIdx, setActiveIdx] = useState(null)
 
+    return (
+        <div className="home">
 
-const Home=()=>{
-    const [activeIdx,setActiveIdx]=useState(null)
-    // const activeElement=(e)=>{
-    //     // return activeIdx
-    //      console.log(activeIdx)
-    // }
-//    console.log(activeIdx)
-    return(
-        <div>
-        {
-         
-            tab.map((e, idx)=>{
-                return (                      
-                        <Tab element={e} index={idx} onClick={setActiveIdx} onChange={activeIdx} />
-  
-               )
-            })
-        }
-            <BookCard/>
+            <div className="home__tab">
+                {
+
+                    tab.map((e, idx) => {
+                        return (
+                            <Tab element={e} index={idx} onClick={setActiveIdx} onChange={activeIdx}/>
+                        )
+                    })
+                }
+
+            </div>
+
+            <hr/>
+
+            <div className="home__books">
+                {
+                    [...Array(5)].map(e => {
+                        return <BookCard/>
+                    })
+                }
+            </div>
         </div>
     )
 }
